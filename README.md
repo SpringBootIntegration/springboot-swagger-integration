372cb4f6f69e3a78700c95383250890c4e1be915
shicheng <shicheng@ttxit.com>
2018-05-09 13:07:17 +0800

    Swagger基础环境配置

---
+++ README.md
@@ -0,0 +1,10 @@
+SpringBoot Swagger Integration
+---
+
+- 相关视频地址
+
+[Spring Boot整合Swagger自动化文档](https://www.edurt.com/course/93)
+
+- 相关文档地址
+
+    - [知识库](http://wiki.ttxit.com/pages/viewpage.action?pageId=16318880)
---
+++ src/main/java/com/edurt/SpringBootSwaggerIntegrationLaunch.java
@@ -0,0 +1,38 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.edurt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBootSwaggerIntegrationLaunch <br/>
+ * 描述 : SpringBootSwaggerIntegrationLaunch <br/>
+ * 作者 : qianmoQ <br/>
+ * 版本 : 1.0 <br/>
+ * 创建时间 : 2018-05-09 上午12:31 <br/>
+ * 联系作者 : <a href="mailTo:shichengoooo@163.com">qianmoQ</a>
+ */
+@SpringBootApplication
+public class SpringBootSwaggerIntegrationLaunch {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwaggerIntegrationLaunch.class, args);
+    }
+
+}
