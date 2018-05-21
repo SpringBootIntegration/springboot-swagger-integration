8a52d9d06e25e6e25b8b7d57cfddbc8ab974c1e1
shicheng <shicheng@ttxit.com>
2018-05-21 22:25:38 +0800

    修改对外API文档描述

--- src/main/java/com/edurt/controller/SwaggerController.java
+++ src/main/java/com/edurt/controller/SwaggerController.java
@@ -17,6 +17,10 @@
  */
 package com.edurt.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +34,16 @@ import org.springframework.web.bind.annotation.RestController;
  * 联系作者 : <a href="mailTo:shichengoooo@163.com">qianmoQ</a>
  */
 @RestController
+@Api(value = "swagger对外api接口", description = "该接口注解主要用于描述swagger的功能点")
 public class SwaggerController {
 
+    @ApiOperation(value = "打印测试swagger信息", notes = "打印测试示例", tags = {"swagger", "info"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 401, message = "未授权"),
+            @ApiResponse(code = 403, message = "拒绝访问"),
+            @ApiResponse(code = 404, message = "资源未找到"),
+            @ApiResponse(code = 200, message = "访问接口成功")
+    })
     @RequestMapping(value = "info", method = RequestMethod.GET)
     String info() {
         return "我是Swagger测试示例";
